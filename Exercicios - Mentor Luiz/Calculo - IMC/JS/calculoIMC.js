5a8e06174f7e321db459f54ac3a135bb4e8966fb
Kaique Xavier <kaiquexatalla@gmail.com>
2017-09-13 18:26:56 -0300

    atualizado calculo do IMC

--- Exercicios - Mentor Luiz/Calculo - IMC/JS/calculoIMC.js
+++ Exercicios - Mentor Luiz/Calculo - IMC/JS/calculoIMC.js
@@ -12,5 +12,21 @@
 
 			 	var imc = peso / (altura * altura);
 
-				document.getElementById("resultadoIMC").innerHTML = (nome + ", o seu IMC é: " + imc);
+			 	if(imc <= 18.5){
+
+			 		document.getElementById("resultadoIMC").innerHTML = (nome + ", o seu IMC é: " + imc + ". Você está abaixo do seu peso ideal!");
+
+			 	} else if (imc > 18.5 && imc <= 25){
+
+			 		document.getElementById("resultadoIMC").innerHTML = (nome + ", o seu IMC é: " + imc + ". Você está no peso ideal!");
+
+			 	} else if (imc > 25 && imc <= 30){
+
+			 		document.getElementById("resultadoIMC").innerHTML = (nome + ", o seu IMC é: " + imc + ". Você está levemente acima do peso ideal!");
+
+			 	} else if(imc > 30){
+
+			 		document.getElementById("resultadoIMC").innerHTML = (nome + ", o seu IMC é: " + imc + ". Você está obeso!");
+
+			 	} 
 			}
